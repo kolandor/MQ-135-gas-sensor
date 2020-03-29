1729bb9ade4026e7390076cc6c255c4b7d042057
tomacamlar <tomacamlar@protonmail.com>
2020-03-29 22:23:59 +0200

    Add class methods

--- MQ-135 gas sensor/lib/MQSensor/MQSensor.h
+++ MQ-135 gas sensor/lib/MQSensor/MQSensor.h
@@ -10,9 +10,27 @@ private:
 
     const int ACCURANCY = 5;
 
+    const size_t _checkIterationCount = 60;
+
+    void setDungeourLevel(const int &valueFromMq)
+    {
+        for (size_t i = 1; i <= 5; i++)
+        {
+            digitalWrite(i + 1, valueFromMq - _normalizedMinimalValue >= (i * ACCURANCY));
+        }
+    }
+
+    void setAllLEDs(const bool &state)
+    {
+        for (size_t i = 1; i <= 5; i++)
+        {
+            digitalWrite(i + 1, state);
+        }
+    }
+
 public:
 
-    MQSensor(int &analogPin):_analogPin(analogPin), _normalizedMinimalValue(20)
+    MQSensor(const int &analogPin):_analogPin(analogPin), _normalizedMinimalValue(20)
     {
         for (size_t pin = 2; pin <= 6; pin++)
         {
@@ -20,11 +38,30 @@ public:
         }
     }
 
-    void setDungeourLevel(int &valueFromMq)
+    void displayDungeourLevel()
     {
-        for (size_t i = 1; i <= 5; i++)
+        setDungeourLevel(getSensorLevel());
+    }
+
+    int getSensorLevel()
+    {
+        return analogRead(_analogPin);
+    }
+
+    void calibrateSensor()
+    {
+        int sensorSum = 0;
+
+        for (size_t i = 0; i < _checkIterationCount; i++)
         {
-            digitalWrite(i + 1, valueFromMq - _normalizedMinimalValue >= (i * ACCURANCY));
+            delay(300);
+            setAllLEDs(i % 2);
+            delay(300);
+            sensorSum += getSensorLevel();
         }
+
+        _normalizedMinimalValue = sensorSum / _checkIterationCount - ACCURANCY;
+
+        setAllLEDs(false);
     }
 };
