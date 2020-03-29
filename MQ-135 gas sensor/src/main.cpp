ee7244b33752b79de06fbb08910c4e0976d33a1d
tomacamlar <tomacamlar@protonmail.com>
2020-03-29 21:38:56 +0200

    Add sensor code

--- MQ-135 gas sensor/src/main.cpp
+++ MQ-135 gas sensor/src/main.cpp
@@ -1,9 +1,33 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+int normalizedMinimalValue = 20;
+const int accuracy = 5;
+
+void setDungeourLevel(int &valueFromMq)
+{
+  for (size_t i = 1; i <= 5; i++)
+  {
+    digitalWrite(i + 1, valueFromMq - normalizedMinimalValue >= (i * accuracy));
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  for (size_t pin = 2; pin <= 6; pin++)
+  {
+    pinMode(pin, OUTPUT);
+  }
+
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+  delay(600);
+  int sensorValue = analogRead(A0);
+  Serial.print("sensor = ");
+  Serial.print(sensorValue);
+  Serial.print("\n");
+  setDungeourLevel(sensorValue);
 }
